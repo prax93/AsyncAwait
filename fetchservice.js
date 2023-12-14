fd1ba4be486bb44239cc3dbbb2c3f829d3578c3a
Prashanth Sritharan <prashanth.sritharan@sbb.ch>
2023-12-14 13:12:47 +0100

    Added Env Variable for Api-Key

--- fetchservice.js
+++ fetchservice.js
@@ -1,8 +1,9 @@
+let api_key = process.env.api_key;
 
 export async function getMoviesAndStoreThem() {
 
     try {
-        let fetchedMovies = await fetch('https://api.themoviedb.org/3/trending/all/week?api_key=xxx')
+        let fetchedMovies = await fetch('https://api.themoviedb.org/3/trending/all/week?api_key='+api_key)
 
         if (fetchedMovies.ok){
 
